cb9ae656fdc522884c85989265f5355382275c45
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-18 02:34:48 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  # # your code here
+  # numbers = []
+  # numbers.each do |number| **2
+  # new_array << number ** 2
+  def square_array(array)
+  squared = []
+  array.each { |num| squared << num**2 }
+  squared
+end
 end
